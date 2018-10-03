8a4d96aa51b8f26ff424ac4adbcea5641b20b531
Jens Nielsen <nls@ssi.dk>
2018-10-03 15:16:21 +0200

    Update package

--- Deploy to WEB.R
+++ Deploy to WEB.R
@@ -1,10 +1,15 @@
-
+#
 setwd("H:/SFSD/INFEPI/Projekter/AKTIVE/MOMO/GitHub/EuroMOMOnetwork")
 
 devtools::document("MOMO")
 devtools::check("MOMO")
-file <- devtools::build("MOMO")
 
+# Creates a source package
+file <- devtools::build("MOMO", binary = FALSE)
+drat::insertPackage(file, repodir = "drat")
+
+# Creates a Windows binary package
+file <- devtools::build("MOMO", binary = TRUE)
 drat::insertPackage(file, repodir = "drat")
 
 saveRDS(read.dcf("drat/src/contrib/PACKAGES"), "drat/src/contrib/PACKAGES.rds")
